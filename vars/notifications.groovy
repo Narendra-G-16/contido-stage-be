71ec669df97cf3dc6c4471915221f362cca4fbba
Narendra G <narendra@planetc.net>
2026-03-24 18:54:40 +0530

    Refactor notification function for clarity and efficiency

--- vars/notifications.groovy
+++ vars/notifications.groovy
@@ -1,41 +1,44 @@
-def call() {
+// vars/notifications.groovy
+
 def call(String buildStatus = 'STARTED') {
-  buildStatus =  buildStatus ?: 'SUCCESSFUL'
-  def jobUserId, jobUserName
-//get user id and name of the build started user.
-  wrap([$class: 'BuildUser']) {
-      jobUserId = "${BUILD_USER_ID}"
-      jobUserName = "${BUILD_USER}"
-}
+    // Default build status if null
+    buildStatus = buildStatus ?: 'SUCCESSFUL'
 
-  println("Started By: ${jobUserName}")
-  def colorName = 'RED'
-  def colorCode = '#FF0000'
-  def summary = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL} env: `stage` triggered by: `${jobUserId}`"
+    def jobUserId, jobUserName
 
-  // Override default values based on build status
-  if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
-    colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESSFUL') {
-    color = 'GREEN'
-    colorCode = '#00FF00'
-  } else {
-    color = 'RED'
-    colorCode = '#FF0000'
-  }
+    // Get user id and name of the build started user
+    wrap([$class: 'BuildUser']) {
+        jobUserId = "${BUILD_USER_ID}"
+        jobUserName = "${BUILD_USER}"
+    }
 
-  // Send notifications
-   slackSend (color: colorCode, message: summary)
-  
-//   // Send email notifications
-      emailext (
-      subject: "$buildStatus : Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-      mimeType: 'text/html',
-      body: """<p>$buildStatus : Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'</p>
-        <p>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a></p>""",
-    //  recipientProviders: [developers(), requestor()],
-      to: 'narendra@planetc.net,suryanarayana@planetc.net,mounikakondeti@planetc.net')
+    println("Started By: ${jobUserName}")
 
-}
+    // Default notification color
+    def color = 'RED'
+    def colorCode = '#FF0000'
+
+    // Prepare summary
+    def summary = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL} env: `stage` triggered by: `${jobUserId}`"
+
+    // Override color based on build status
+    if (buildStatus == 'STARTED') {
+        color = 'YELLOW'
+        colorCode = '#FFFF00'
+    } else if (buildStatus == 'SUCCESSFUL') {
+        color = 'GREEN'
+        colorCode = '#00FF00'
+    }
+
+    // Send Slack notification
+    slackSend(color: colorCode, message: summary)
+
+    // Send email notification
+    emailext(
+        subject: "$buildStatus : Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+        mimeType: 'text/html',
+        body: """<p>$buildStatus : Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'</p>
+                 <p>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a></p>""",
+        to: 'narendra@planetc.net,suryanarayana@planetc.net,mounikakondeti@planetc.net'
+    )
 }
