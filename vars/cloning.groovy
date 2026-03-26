40106779d298962d7820e9c1280282d23b16da0b
Narendra G <narendra@planetc.net>
2026-03-26 12:03:16 +0530

    Update and rename buildimage.groovy to cloning.groovy

---
+++ vars/cloning.groovy
@@ -0,0 +1,11 @@
+def call() {        
+    notifyBuild('STARTED')
+        
+    stage('checkout-code'){
+        
+        println "======================================== \n     Cloning the git repo... \n========================================"
+        
+        checkout([$class: 'GitSCM', branches: [[name: "*/$branch"]], extensions: [], userRemoteConfigs: [[credentialsId: 'suryadesy_github', url: 'https://github.com/Desynova/Contido.git']]])
+    }
+	
+   
