d9e1cbd54e3dcef11f66a4c2cd25ac4f39b99da4
Narendra G <narendra@planetc.net>
2026-03-26 12:14:00 +0530

    Add branch parameter to cloning function
    Modified the call function to accept a branch parameter for checkout.

--- vars/cloning.groovy
+++ vars/cloning.groovy
@@ -1,8 +1,9 @@
-def call() {        
+def call(String branch) {
+
     notifyBuild('STARTED')
-        
-    stage('checkout-code'){
-        
+
+    stage('checkout-code') {
+		
         println "======================================== \n     Cloning the git repo... \n========================================"
         
         checkout([$class: 'GitSCM', branches: [[name: "*/$branch"]], extensions: [], userRemoteConfigs: [[credentialsId: 'suryadesy_github', url: 'https://github.com/Desynova/Contido.git']]])
