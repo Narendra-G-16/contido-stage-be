fee7ed69697ffe7b7af67aa1d6f938c7c773bd91
Narendra G <narendra@planetc.net>
2026-03-31 11:33:36 +0530

    Rename notifyBuild function to call

--- vars/notifications.groovy
+++ vars/notifications.groovy
@@ -1,4 +1,4 @@
-def notifyBuild (String buildStatus = 'STARTED') {
+def call (String buildStatus = 'STARTED') {
   buildStatus =  buildStatus ?: 'SUCCESSFUL'
  // def jobUserId, jobUserName
 //get user id and name of the build started user.
